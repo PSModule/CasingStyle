b8963c4e66e0800bc2f6329ca5c9d2d979cea327
Marius Storhaug <marstor@hotmail.com>
2025-02-02 14:42:17 +0100

    Update README and examples for CasingStyle module: enhance usage instructions and add more examples

--- examples/General.ps1
+++ examples/General.ps1
@@ -1,19 +1,34 @@
-﻿<#
-  .SYNOPSIS
-    This is a general example of how to use the module.
-#>
+﻿# Example usage of Get-CasingStyle
+# Detects the casing style of various strings
 
-# Import the module
-Import-Module -Name 'PSModule'
+'testtesttest' | Get-CasingStyle  # lowercase
+'TESTTESTTEST' | Get-CasingStyle  # UPPERCASE
+'Testtesttest' | Get-CasingStyle  # Sentencecase
+'TestTestTest' | Get-CasingStyle  # PascalCase
+'testTestTest' | Get-CasingStyle  # camelCase
+'test-test-test' | Get-CasingStyle  # kebab-case
+'TEST-TEST-TEST' | Get-CasingStyle  # UPPER-KEBAB-CASE
+'test_test_test' | Get-CasingStyle  # snake_case
+'TEST_TEST_TEST' | Get-CasingStyle  # UPPER_SNAKE_CASE
+'Test_teSt-Test' | Get-CasingStyle  # Mixed case with special characters
 
-# Define the path to the font file
-$FontFilePath = 'C:\Fonts\CodeNewRoman\CodeNewRomanNerdFontPropo-Regular.tff'
+# Example usage of ConvertTo-CasingStyle
+# Converts a string to different casing styles
 
-# Install the font
-Install-Font -Path $FontFilePath -Verbose
+'thisIsCamelCase' | ConvertTo-CasingStyle -To 'snake_case'  # Converts to 'this_is_camel_case'
+'thisIsCamelCase' | ConvertTo-CasingStyle -To 'UPPER_SNAKE_CASE'  # Converts to 'THIS_IS_CAMEL_CASE'
+'thisIsCamelCase' | ConvertTo-CasingStyle -To 'kebab-case'  # Converts to 'this-is-camel-case'
 
-# List installed fonts
-Get-Font -Name 'CodeNewRomanNerdFontPropo-Regular'
+# Example usage of Split-CasingStyle
+# Splits a string based on different casing styles
 
-# Uninstall the font
-Get-Font -Name 'CodeNewRomanNerdFontPropo-Regular' | Uninstall-Font -Verbose
+Split-CasingStyle -Text 'this-is-a-kebab-case-string' -By 'kebab-case'  # Splits into words
+Split-CasingStyle -Text 'this_is_a_kebab_case_string' -By 'snake_case'  # Splits into words
+Split-CasingStyle -Text 'ThisIsAPascalCaseString' -By 'PascalCase'  # Splits into words
+Split-CasingStyle -Text 'thisIsACamelCaseString' -By 'camelCase'  # Splits into words
+
+# Chained splitting example
+Split-CasingStyle -Text 'this_is_a-CamelCaseString' -By kebab-case | Split-CasingStyle -By snake_case  # Chained splitting
+
+# Piped example with multiple styles
+'this_is_a-PascalString' | Split-CasingStyle -By 'snake_case', 'kebab-case', 'PascalCase'
