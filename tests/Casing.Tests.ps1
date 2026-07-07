8f88168ef8c7aa31ce72e537d7ac5662e79dddc5
Marius Storhaug <marstor@hotmail.com>
2026-07-07 17:41:02 +0200

    Require Pester 6.x in test files

--- tests/Casing.Tests.ps1
+++ tests/Casing.Tests.ps1
@@ -1,4 +1,6 @@
-﻿Describe 'CasingStyle' {
+﻿#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '6.0.0'; MaximumVersion = '6.*'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
+
+Describe 'CasingStyle' {
     Context 'Function: Get-CasingStyle' {
         It "Get-CasingStyle: Detects 'testtesttest' as lowercase" {
             'testtesttest' | Get-CasingStyle | Should -Be 'lowercase'
--- tests/UseCases.Tests.ps1
+++ tests/UseCases.Tests.ps1
@@ -1,4 +1,6 @@
-﻿#--------------------------------------------------------------------
+﻿#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '6.0.0'; MaximumVersion = '6.*'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
+
+#--------------------------------------------------------------------
 # Test Get-CasingStyle: verify that known inputs are detected correctly,
 # and that ambiguous strings return 'Unknown'
 #--------------------------------------------------------------------
